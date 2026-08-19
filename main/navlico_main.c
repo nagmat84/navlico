1e4f1d94ab40c943eacea19d15425732c9e0262c
Matthias Nagel <matthias.h.nagel@posteo.de>
2026-08-19 20:20:21 +0200

    Added options for min/max CPU frequency

--- main/navlico_main.c
+++ main/navlico_main.c
@@ -76,10 +76,10 @@ void log_light_sleep_duration( void ) {
  * Also see [Espressif: API Guides - Low Power Modes - DFS Configuration](https://docs.espressif.com/projects/esp-idf/en/v6.0.2/esp32h2/api-guides/low-power-mode/low-power-mode-soc.html#dfs-configuration).
  */
 void setup_power_management( void ) {
-	ESP_LOGI( LOG_TAG, "Enabling DFS with %u MHz as minimum frequency", CONFIG_NAVLICO_MIN_FREQ );
+	ESP_LOGI( LOG_TAG, "Enabling DFS between %u	MHz and %u MHz", CONFIG_NAVLICO_PM_MIN_FREQ, CONFIG_NAVLICO_PM_MAX_FREQ );
 	const esp_pm_config_t pm_config = {
-		.max_freq_mhz = CONFIG_ESP_DEFAULT_CPU_FREQ_MHZ,
-		.min_freq_mhz = CONFIG_NAVLICO_MIN_FREQ,
+		.max_freq_mhz = CONFIG_NAVLICO_PM_MAX_FREQ,
+		.min_freq_mhz = CONFIG_NAVLICO_PM_MIN_FREQ,
 		.light_sleep_enable = true
 	};
 	ESP_ERROR_CHECK( esp_pm_configure( &pm_config ) );
